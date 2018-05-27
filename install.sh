4d8946e59b76fc1013f108f68b3b004b94cd1433
Mark Skilbeck <markskilbeck@gmail.com>
2018-05-27 11:03:48 +0100

    Add comments to install script

--- install.sh
+++ install.sh
@@ -4,10 +4,19 @@
 # HOME="/home/mgsk/HOME2"
 # mkdir -p "$HOME"
 
+# Read all file paths in dotfiles directory, ignoring git files, this
+# file, and "." (current dir).
 find | grep -v "^./.git" | grep -v "install.sh" | egrep -v "^\./?$" | while read x; do
+    # Cut off the first 2 characters ("./")
     f=$(echo "$x" | cut -c "3-")
+    # If the path is a directory, create a directory of same name in
+    # our home dir, and then continue on to next path (skip rest of
+    # this)
     [ -d "$x" ] && mkdir -vp "$HOME/$f" && echo "$HOME/$f" && continue
+    # If a symbolic link or file with the same path in the home dir,
+    # copy it to a back up, and remove the original
     [ -L "$HOME/$f" ] || [ -f "$HOME/$f" ] && cp "$HOME/$f" "$HOME/$f-backup.$(date +%s)" \
         && rm "$HOME/$f" && echo "Found symbolic link or file $HOME/$f; backed up"
+    # Create symbolic link in home dir to the dotfiles path
     ln -s "$(pwd)/$x" "$HOME/$f"
 done 
